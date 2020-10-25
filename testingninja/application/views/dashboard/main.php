7510daf6b97c5153e6a3df913aea133c4ee7db72
Maizatul Fatihah <norzaitolhanim74@gmail.com>
2020-10-25 14:25:23 +0800

    creating features for dashboard and configure system

---
+++ testingninja/application/views/dashboard/main.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="admin.css">
+    <script src="https://kit.fontawesome.com/6fa553b1a3.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="fontawesome-free/css/all.min.css">
+
+    <title>Ninja Referral</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+      <a class="navbar-brand" href="#" ><h4 class="text-danger">NINJA VAN</h4></a>
+      
+        <form class="form-inline my-2 my-lg-0 ml-auto">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-dark my-2 my-sm-0" type="submit">Search</button>
+        </form>
+
+        <div class="icon ml-4">
+          <h5>
+            <i class="fas fa-envelope mr-3" data-toggle="tooltip" title="Direct Message"></i>
+            <i class="fas fa-bell mr-3" data-toggle="tooltip" title="Notification"></i>
+            <i class="fas fa-sign-out-alt mr-3" data-toggle="tooltip" title="Log Out"></i>
+          </h5>
+        </div>
+    </nav>
+
+    <div class="row no gutters mt-5">
+      <div class=" col-md-3 mt-2 bg-dark pr-3 pt-4">
+        <ul class="nav flex-column ml-3 mb-4">
+          <li class="nav-item">
+            <a class="nav-link active text-white" href="#"><i class="fas fa-columns mr-2"></i>Dashboard</a><hr class="bg-secondary">
+          </li>
+              <li class="nav-item">
+                <a class="nav-link text-white" href="#">Refer A Friend</a><hr class="bg-secondary">
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-white" href="#">Friend List</a><hr class="bg-secondary">
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-white" href="#">Commission</a><hr class="bg-secondary">
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-white" href="#">Setting</a><hr class="bg-secondary">
+              </li>
+          </ul>
+        </div>
+        <div class="col-md-10 p-5 pt-2">
+          <div class="row">
+            <div class="card" style="width: 18rem;">
+              <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+      <!-- Optional JavaScript; choose one of the two! -->
+
+      <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+      <script type="text/javascript" src="admin.js"></script>
+
+
+  </body>
+</html>
